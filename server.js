018ad8eb80b02bf0e7f7cba38c9d12b7181ebc51
Martin Facciuto <martin_facciuto@hotmail.com>
2025-03-27 16:04:01 -0300

    add Prettier configuration and ignore files

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const { APP_PORT } = process.env;
+const { APP_PORT } = process.env
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(APP_PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${APP_PORT}`);
-});
+  console.log(`Servidor corriendo en http://localhost:${APP_PORT}`)
+})
