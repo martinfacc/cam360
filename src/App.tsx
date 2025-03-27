334476f57f656a35c40e0aaca78526f2a545aff2
Martin Facciuto <martin_facciuto@hotmail.com>
2025-03-27 20:53:36 -0300

    update GyroScene to adjust camera rotation order for correct orientation handling

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ const GyroScene = () => {
         const beta = event.beta ? THREE.MathUtils.degToRad(event.beta) : 0 // Rotación sobre el eje X (arriba/abajo)
         const gamma = event.gamma ? THREE.MathUtils.degToRad(event.gamma) : 0 // Rotación sobre el eje Y (lateral)
 
-        camera.rotation.set(alpha, beta, gamma)
+        camera.rotation.set(beta, alpha, gamma)
 
         // const targetQuaternion = new THREE.Quaternion().setFromEuler(
         //   new THREE.Euler(beta, alpha, -gamma, 'XYZ')
